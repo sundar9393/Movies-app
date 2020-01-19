08db82836626a0cf0bd44809302c331b1aba4d61
sundar93 <sundar9393@gmail.com>
2020-01-19 18:22:55 +0530

    modal styling stuff

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,12 +3,26 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)'
+    }
+};
 
 class Header extends Component {
     constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value: 0
         }
     }
     openModalHandler = () => {
@@ -17,6 +31,10 @@ class Header extends Component {
 
     closeModalHandler = () => {
         this.setState({modalIsOpen: false})
+    }
+
+    tabChangeHandler = (event, value) => {
+        this.setState({value})
     }
     render() {
         return (
@@ -28,10 +46,12 @@ class Header extends Component {
                     </div>
                 </header>
                 <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" 
-                    onRequestClose={this.closeModalHandler}>
-
-                    </Modal>
-                
+                    onRequestClose={this.closeModalHandler} style={customStyles}>
+                        <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                            <Tab label="Login" />
+                            <Tab label="Register" />
+                        </Tabs>
+                </Modal>
             </div>
         )
     }
